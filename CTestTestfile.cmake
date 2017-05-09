600d7bb1d577e576120d02c0a46f9718b51047d9
Markus <markus.hadenfeldt@web.de>
2017-05-09 16:06:07 +0200

    Remove unused files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wolverindev/git/libnbtplusplus
-# Build directory: /home/wolverindev/git/libnbtplusplus
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
